4ef525018b261e747db2a2edf24e0ea8932e77d8
Octavian Geagla <ogeagla@gmail.com>
2015-10-31 11:40:05 -0600

    refactor and wip compositor

--- src/main/scala/com/oct/ThingDoer.scala
+++ src/main/scala/com/oct/ThingDoer.scala
@@ -1,155 +1,165 @@
 package com.oct
 
 import com.sksamuel.scrimage.filter.GrayscaleFilter
-import com.sksamuel.scrimage.{ScaleMethod, Image}
-import org.slf4j.LoggerFactory
+import com.sksamuel.scrimage.{Image, ScaleMethod}
 
 import scala.util.Random
 
-object ThingDoer {
-
-  lazy val log = LoggerFactory.getLogger(getClass)
-
-  trait ImageManipulator {
-    def apply(img: Image): Image
+object TilesStuff {
+  def apply(ncol: Int, nrow: Int, colW: Int, rowH: Int, imgs: List[List[Image]]): Image = {
+    
+    ???
   }
+}
 
-  trait Similarity {
-    def apply(img1: Image, img2: Image): Double
-  }
+object UniqueCartesian2 {
+  def apply(imgs1: List[Image], imgs2: List[Image]): List[(Image, Image)] = {
+    var array = List[(Image, Image)]()
 
-  object Manipulate {
-    def apply(img: Image, manips: List[ImageManipulator]): Image = {
-      manips.foldLeft(img)((image, maniper) => maniper(image))
-    }
-  }
+    for (i1 <- imgs1) {
+      for (i2 <- imgs2) {
+        val tup = (i1, i2)
+        val put = (i2, i1)
 
-  object MixManipulations {
-    def apply(man1: List[ImageManipulator], man2: List[ImageManipulator]): List[ImageManipulator] = {
-      val slice1 = new Random().nextInt(man1.length)
-      val slice2 = new Random().nextInt(man2.length)
+        if (array.contains(tup) || array.contains(put) || i1 == i2) {
 
-      val flipper = new Random().nextBoolean()
+        } else {
+          array = tup :: array
+        }
 
-      val (man1b, man2b) = flipper match {
-        case true =>
-          val a = man1.slice(0, slice1)
-          val b = man2.slice(slice2, man2.length)
-          (a, b)
-        case false =>
-          val a = man1.slice(slice1, man1.length)
-          val b = man2.slice(0, slice2)
-          (a, b)
       }
-
-      man1b ++: man2b
     }
-  }
 
-  class ManipulatorOne extends ImageManipulator {
-    override def apply(img: Image): Image = {
-      img.flipX
+    array
+  }
+}
+
+trait ImageManipulator {
+  def apply(img: Image): Image
+}
+
+trait Similarity {
+  def apply(img1: Image, img2: Image, scaleWidth: Int, scaleHeight: Int): Double
+}
+
+object Manipulate {
+  def apply(img: Image, manips: List[ImageManipulator]): Image = {
+    manips.foldLeft(img)((image, maniper) => maniper(image))
+  }
+}
+
+object MixManipulations {
+  def apply(man1: List[ImageManipulator], man2: List[ImageManipulator]): List[ImageManipulator] = {
+    val slice1 = new Random().nextInt(man1.length)
+    val slice2 = new Random().nextInt(man2.length)
+
+    val flipper = new Random().nextBoolean()
+
+    val (man1b, man2b) = flipper match {
+      case true =>
+        val a = man1.slice(0, slice1)
+        val b = man2.slice(slice2, man2.length)
+        (a, b)
+      case false =>
+        val a = man1.slice(slice1, man1.length)
+        val b = man2.slice(0, slice2)
+        (a, b)
     }
+
+    man1b ++: man2b
   }
+}
 
-  object Distance2 {
-    def apply(arr1: List[Double], arr2: List[Double]): Double = {
-      assert(arr1.length == arr2.length)
-      math.sqrt(arr1.zip(arr2).map{case (a, b) => math.pow(a-b, 2.0)}.foldLeft(0.0)(_ + _))
-    }
+class ManipulatorOne extends ImageManipulator {
+  override def apply(img: Image): Image = {
+    img.flipX
   }
+}
 
-  object ImageSimilarityArgbDistance2 {
-    def apply(img1: Image, img2: Image): Double = (new ImageSimilarityArgbDistance2)(img1, img2)
+object Distance2 {
+  def apply(arr1: List[Double], arr2: List[Double]): Double = {
+    assert(arr1.length == arr2.length)
+    math.sqrt(arr1.zip(arr2).map{case (a, b) => math.pow(a-b, 2.0)}.foldLeft(0.0)(_ + _))
   }
+}
 
-  class ImageSimilarityArgbDistance2 extends Similarity {
-    def apply(img1: Image, img2: Image): Double = {
+object ImageSimilarityArgbDistance2 {
+  def apply(img1: Image, img2: Image): Double = (new ImageSimilarityArgbDistance2)(img1, img2)
+}
 
-      log.info(s"getting similarity")
+class ImageSimilarityArgbDistance2 extends Similarity {
+  def apply(img1: Image, img2: Image, scaleWidth: Int = 32, scaleHeight: Int = 32): Double = {
 
-      val argb1: Array[Array[Int]] = img1.scaleTo(32, 32, ScaleMethod.FastScale).argb
-      val argb2: Array[Array[Int]] = img2.scaleTo(32, 32, ScaleMethod.FastScale).argb
+    val argb1: Array[Array[Int]] = img1.scaleTo(scaleWidth, scaleHeight, ScaleMethod.FastScale).argb
+    val argb2: Array[Array[Int]] = img2.scaleTo(scaleWidth, scaleHeight, ScaleMethod.FastScale).argb
 
-      val a1 = argb1.map(_.apply(0).toDouble).toList
-      val r1 = argb1.map(_.apply(1).toDouble).toList
-      val g1 = argb1.map(_.apply(2).toDouble).toList
-      val b1 = argb1.map(_.apply(3).toDouble).toList
+    val a1 = argb1.map(_.apply(0).toDouble).toList
+    val r1 = argb1.map(_.apply(1).toDouble).toList
+    val g1 = argb1.map(_.apply(2).toDouble).toList
+    val b1 = argb1.map(_.apply(3).toDouble).toList
 
-      val a2 = argb2.map(_.apply(0).toDouble).toList
-      val r2 = argb2.map(_.apply(1).toDouble).toList
-      val g2 = argb2.map(_.apply(2).toDouble).toList
-      val b2 = argb2.map(_.apply(3).toDouble).toList
+    val a2 = argb2.map(_.apply(0).toDouble).toList
+    val r2 = argb2.map(_.apply(1).toDouble).toList
+    val g2 = argb2.map(_.apply(2).toDouble).toList
+    val b2 = argb2.map(_.apply(3).toDouble).toList
 
-      val aDist = Distance2(a1, a2)
-      val rDist = Distance2(r1, r2)
-      val gDist = Distance2(g1, g2)
-      val bDist = Distance2(b1, b2)
+    val aDist = Distance2(a1, a2)
+    val rDist = Distance2(r1, r2)
+    val gDist = Distance2(g1, g2)
+    val bDist = Distance2(b1, b2)
 
-      math.sqrt(aDist*aDist + rDist*rDist + gDist*gDist + bDist*bDist)
-    }
+    math.sqrt(aDist*aDist + rDist*rDist + gDist*gDist + bDist*bDist)
   }
+}
 
-  object ImageSimilarityRgbDistance2 {
-    def apply(img1: Image, img2: Image): Double = (new ImageSimilarityRgbDistance2)(img1, img2)
+object ImageSimilarityRgbDistance2 {
+  def apply(img1: Image, img2: Image): Double = (new ImageSimilarityRgbDistance2)(img1, img2)
+}
+
+class ImageSimilarityRgbDistance2 extends Similarity {
+  def apply(img1: Image, img2: Image, scaleWidth: Int = 32, scaleHeight: Int = 32): Double = {
+
+    val rgb1: Array[Array[Int]] = img1.scaleTo(scaleWidth, scaleHeight, ScaleMethod.FastScale).rgb
+    val rgb2: Array[Array[Int]] = img2.scaleTo(scaleWidth, scaleHeight, ScaleMethod.FastScale).rgb
+
+    val r1 = rgb1.map(_.apply(0).toDouble).toList
+    val g1 = rgb1.map(_.apply(1).toDouble).toList
+    val b1 = rgb1.map(_.apply(2).toDouble).toList
+
+    val r2 = rgb2.map(_.apply(0).toDouble).toList
+    val g2 = rgb2.map(_.apply(1).toDouble).toList
+    val b2 = rgb2.map(_.apply(2).toDouble).toList
+
+    val rDist = Distance2(r1, r2)
+    val gDist = Distance2(g1, g2)
+    val bDist = Distance2(b1, b2)
+
+    math.sqrt(rDist*rDist + gDist*gDist + bDist*bDist)
   }
+}
 
-  class ImageSimilarityRgbDistance2 extends Similarity {
-    def apply(img1: Image, img2: Image): Double = {
+object ImageSimilarityGrayscaleDistance2 {
+  def apply(img1: Image, img2: Image): Double = (new ImageSimilarityGrayscaleDistance2)(img1, img2)
+}
 
-      log.info(s"getting similarity")
+class ImageSimilarityGrayscaleDistance2 extends Similarity {
+  def apply(img1: Image, img2: Image, scaleWidth: Int = 32, scaleHeight: Int = 32): Double = {
 
-      val rgb1: Array[Array[Int]] = img1.scaleTo(32, 32, ScaleMethod.FastScale).rgb
-      val rgb2: Array[Array[Int]] = img2.scaleTo(32, 32, ScaleMethod.FastScale).rgb
+    val rgb1: Array[Array[Int]] = img1.scaleTo(scaleWidth, scaleHeight, ScaleMethod.FastScale).filter(GrayscaleFilter).rgb
+    val rgb2: Array[Array[Int]] = img2.scaleTo(scaleWidth, scaleHeight, ScaleMethod.FastScale).filter(GrayscaleFilter).rgb
 
-      val r1 = rgb1.map(_.apply(0).toDouble).toList
-      val g1 = rgb1.map(_.apply(1).toDouble).toList
-      val b1 = rgb1.map(_.apply(2).toDouble).toList
+    val r1 = rgb1.map(_.apply(0).toDouble).toList
+    val g1 = rgb1.map(_.apply(1).toDouble).toList
+    val b1 = rgb1.map(_.apply(2).toDouble).toList
 
-      val r2 = rgb2.map(_.apply(0).toDouble).toList
-      val g2 = rgb2.map(_.apply(1).toDouble).toList
-      val b2 = rgb2.map(_.apply(2).toDouble).toList
+    val r2 = rgb2.map(_.apply(0).toDouble).toList
+    val g2 = rgb2.map(_.apply(1).toDouble).toList
+    val b2 = rgb2.map(_.apply(2).toDouble).toList
 
-      val rDist = Distance2(r1, r2)
-      val gDist = Distance2(g1, g2)
-      val bDist = Distance2(b1, b2)
+    val rDist = Distance2(r1, r2)
+    val gDist = Distance2(g1, g2)
+    val bDist = Distance2(b1, b2)
 
-      math.sqrt(rDist*rDist + gDist*gDist + bDist*bDist)
-    }
+    math.sqrt(rDist*rDist + gDist*gDist + bDist*bDist)
   }
-
-  object ImageSimilarityGrayscaleDistance2 {
-    def apply(img1: Image, img2: Image): Double = (new ImageSimilarityGrayscaleDistance2)(img1, img2)
-  }
-
-  class ImageSimilarityGrayscaleDistance2 extends Similarity {
-    def apply(img1: Image, img2: Image): Double = {
-
-      log.info(s"getting similarity")
-
-      val rgb1: Array[Array[Int]] = img1.scaleTo(32, 32, ScaleMethod.FastScale).filter(GrayscaleFilter).rgb
-      val rgb2: Array[Array[Int]] = img2.scaleTo(32, 32, ScaleMethod.FastScale).filter(GrayscaleFilter).rgb
-
-      val r1 = rgb1.map(_.apply(0).toDouble).toList
-      val g1 = rgb1.map(_.apply(1).toDouble).toList
-      val b1 = rgb1.map(_.apply(2).toDouble).toList
-
-      val r2 = rgb2.map(_.apply(0).toDouble).toList
-      val g2 = rgb2.map(_.apply(1).toDouble).toList
-      val b2 = rgb2.map(_.apply(2).toDouble).toList
-
-      val rDist = Distance2(r1, r2)
-      val gDist = Distance2(g1, g2)
-      val bDist = Distance2(b1, b2)
-
-      math.sqrt(rDist*rDist + gDist*gDist + bDist*bDist)
-    }
-  }
-
-
-
-  def doIt() = {
-
-  }
-
 }
--- src/test/scala/com/oct/MainTest.scala
+++ src/test/scala/com/oct/MainTest.scala
@@ -2,45 +2,52 @@ package com.oct
 
 import java.io.File
 
-import com.oct.ThingDoer.ImageSimilarityArgbDistance2
 import com.sksamuel.scrimage.nio.JpegWriter
 import com.sksamuel.scrimage.{Image, Position, ScaleMethod}
 import org.scalatest.{BeforeAndAfter, FunSuite, Matchers}
 
 class MainTest extends FunSuite with BeforeAndAfter with Matchers {
 
-  test("stuff works") {
+  test("builds similar composites") {
 
     implicit val writer = JpegWriter.Default
 
     val folder = new File(getClass.getResource("/bap-images").getPath)
-    val files = folder.listFiles().filter(_.isFile).take(200)
+    val files = folder.listFiles().filter(_.isFile).take(400)
+
+    val filesHead = files.head
+    val filesTail = files.tail
+
+    val controlImage = Image.fromFile(filesHead)
+    val controlSize = (controlImage.width, controlImage.height)
+
+    val cols = 8
+    val rows = 8
+
+    val colWidth = controlSize._1 / cols
+    val rowHeight = controlSize._2 / rows
+
+    val images = filesTail.map(f => Image.fromFile(f).scaleTo(colWidth, rowHeight, ScaleMethod.FastScale))
+  }
+
+  ignore("similarity pairs") {
+
+    implicit val writer = JpegWriter.Default
+
+    val folder = new File(getClass.getResource("/bap-images").getPath)
+    val files = folder.listFiles().filter(_.isFile).take(400)
 
     val images = files.map(f => Image.fromFile(f).scaleTo(256, 256, ScaleMethod.FastScale))
 
-    var array = List[(Image, Image)]()
+    val array = UniqueCartesian2(images.toList, images.toList)
 
-    for (i1 <- images) {
-      for (i2 <- images) {
-        val tup = (i1, i2)
-        val put = (i2, i1)
-
-        if (array.contains(tup) || array.contains(put) || i1 == i2) {
-
-        } else {
-          array = tup :: array
-        }
-
-      }
-    }
-    
     val distances = array.par.map { case (i1, i2) => (i1, i2, ImageSimilarityArgbDistance2(i1, i2)) }.toList
 
     val distancesSorted = distances.sortBy{ case (i1, i2, dist) => dist }
 
-    val topFew = distancesSorted.take(5)
+    val topFew = distancesSorted.take(15)
 
-    val bottomFew = distancesSorted.takeRight(5)
+    val bottomFew = distancesSorted.takeRight(15)
 
     topFew.++(bottomFew).foreach { case (i1, i2, dist) =>
       val outPath = getClass.getResource("/").getPath
