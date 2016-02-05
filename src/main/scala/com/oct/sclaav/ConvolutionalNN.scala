633dc4203f54b6c2ccf7e4190c753dd16fb347d3
Octavian Geagla <ogeagla@gmail.com>
2016-02-05 10:51:51 -0700

    wip

--- src/main/scala/com/oct/sclaav/ConvolutionalNN.scala
+++ src/main/scala/com/oct/sclaav/ConvolutionalNN.scala
@@ -17,7 +17,6 @@ import org.slf4j.LoggerFactory
 import scala.collection.JavaConverters._
 
 trait ImageNN {
-  def init(): MultiLayerNetwork
   def train(data: DataSet): Unit
   def apply(data: DataSet): Evaluation[Nothing]
 }
@@ -27,18 +26,55 @@ class ConvolutionalNN(
     numColumns: Int = 2,
     nChannels: Int = 1,
     outputNum: Int = 3,
-    numSamples: Int = 150,
+    numSamples: Int = 10,
     batchSize: Int = 110,
     iterations: Int = 10,
     splitTrainNum: Int = 100,
     seed: Int = 123,
-    listenerFreq: Int = 1) extends ImageNN {
+    listenerFreq: Int = 1
+                     ) extends ImageNN {
 
   val log = LoggerFactory.getLogger(getClass)
 
   val model = init()
 
-  def init(): MultiLayerNetwork = {
+  private def init(): MultiLayerNetwork = {
+
+//    val builderSimple = new NeuralNetConfiguration.Builder()
+//          .seed(123)
+//          .iterations(10)
+//          .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
+//          .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+//          .list(3)
+//          .layer(0, new ConvolutionLayer.Builder(10,10)
+//            .stride(2,2)
+//            .nIn(nChannels)
+//            .nOut(3)
+//            .weightInit(WeightInit.XAVIER)
+//            .activation("relu")
+//            .build())
+//          .layer(1, new SubsamplingLayer.Builder(
+//            SubsamplingLayer.PoolingType.MAX, Array(2,2))
+//            .stride(2,2)
+//            .build())
+//          .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+//            .nOut(3)
+//            .weightInit(WeightInit.XAVIER)
+//            .activation("softmax")
+//            .build())
+//          .backprop(true)
+//          .pretrain(false)
+//
+//        new ConvolutionLayerSetup(builderSimple, 10, 30,
+//          nChannels)    // FIXME: what does this do?
+//
+//        val confSimple = builderSimple.build()
+//
+//        val modelSimple = new MultiLayerNetwork(confSimple)
+//        modelSimple.init()
+//        modelSimple.setListeners(
+//          Seq[IterationListener](new ScoreIterationListener(5)).asJava)// util.Arrays.asList((IterationListener) new ScoreIterationListener(5)))
+//        modelSimple
 
     val builder = new NeuralNetConfiguration.Builder()
       .seed(seed)
--- src/test/scala/com/oct/sclaav/ai/CNNTest.scala
+++ src/test/scala/com/oct/sclaav/ai/CNNTest.scala
@@ -2,7 +2,7 @@ package com.oct.sclaav.ai
 
 import java.util.Random
 
-import com.oct.sclaav.TestHelpers
+import com.oct.sclaav.{ConvolutionalNN, TestHelpers}
 import com.sksamuel.scrimage.Image
 import org.deeplearning4j.datasets.iterator.impl.IrisDataSetIterator
 import org.deeplearning4j.eval.Evaluation
@@ -16,9 +16,10 @@ import org.deeplearning4j.nn.weights.WeightInit
 import org.deeplearning4j.optimize.api.IterationListener
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import org.nd4j.linalg.api.ndarray.INDArray
-import org.nd4j.linalg.dataset.SplitTestAndTrain
+import org.nd4j.linalg.dataset.{DataSet, SplitTestAndTrain}
 import org.nd4j.linalg.factory.Nd4j
 import org.nd4j.linalg.lossfunctions.LossFunctions
+import org.nd4j.linalg.util.FeatureUtil
 import org.scalatest.{BeforeAndAfter, FunSuite, Matchers}
 import org.slf4j.LoggerFactory
 
@@ -26,6 +27,30 @@ import scala.collection.JavaConverters._
 
 
 class CNNTest extends FunSuite with BeforeAndAfter with Matchers with TestHelpers {
+
+  test("my cNN works") {
+
+    val irisIter: IrisDataSetIterator = new IrisDataSetIterator(150, 150)
+    val dataIris: DataSet = irisIter.next()
+    dataIris.normalizeZeroMeanZeroUnitVariance()
+    val trainTest: SplitTestAndTrain = dataIris.splitTestAndTrain(100, new Random(123))
+    val labelsIris = trainTest.getTest().getLabels()
+
+    val cnn = new ConvolutionalNN()
+
+    val nSamples = 10
+    val dataRaw = Nd4j.rand(nSamples,300)
+    val labels = FeatureUtil.toOutcomeMatrix(Array(0,0,0,1,1,1,2,2,2,2),3)
+    val data = new DataSet(dataRaw,labels)
+    data.normalizeZeroMeanZeroUnitVariance()
+
+    cnn.train(data)
+
+//    cnn.printWeights()
+
+    cnn(data)
+
+  }
 
   test("something happens") {
 
@@ -55,9 +80,69 @@ class CNNTest extends FunSuite with BeforeAndAfter with Matchers with TestHelper
       *Set a neural network configuration with multiple layers
       */
 
+
+//    val nChannelsSimple = 1
+//    val nSamples = 10
+//    val data = Nd4j.rand(nSamples,300)
+//    val labels = FeatureUtil.toOutcomeMatrix(Array(0,0,0,1,1,1,2,2,2,2),3)
+//    val d = new DataSet(data,labels)
+//    val builderSimple = new NeuralNetConfiguration.Builder()
+//      .seed(123)
+//      .iterations(10)
+//      .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
+//      .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+//      .list(3)
+//      .layer(0, new ConvolutionLayer.Builder(10,10)
+//        .stride(2,2)
+//        .nIn(nChannelsSimple)
+//        .nOut(3)
+//        .weightInit(WeightInit.XAVIER)
+//        .activation("relu")
+//        .build())
+//      .layer(1, new SubsamplingLayer.Builder(
+//        SubsamplingLayer.PoolingType.MAX, Array(2,2))
+//        .stride(2,2)
+//        .build())
+//      .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+//        .nOut(3)
+//        .weightInit(WeightInit.XAVIER)
+//        .activation("softmax")
+//        .build())
+//      .backprop(true)
+//      .pretrain(false)
+//
+//    new ConvolutionLayerSetup(builderSimple, 10, 30,
+//      nChannelsSimple)    // FIXME: what does this do?
+//
+//    val confSimple = builderSimple.build()
+//
+//    val modelSimple = new MultiLayerNetwork(confSimple)
+//    modelSimple.init()
+//    modelSimple.setListeners(
+//      Seq[IterationListener](new ScoreIterationListener(5)).asJava)// util.Arrays.asList((IterationListener) new ScoreIterationListener(5)))
+//    modelSimple.fit(d)
+//
+//    log.info("Evaluate weights....")
+//    modelSimple.getLayers.foreach { case (layer: org.deeplearning4j.nn.api.Layer) =>
+//      val w: INDArray = layer.getParam(DefaultParamInitializer.WEIGHT_KEY)
+//      log.info("Weights: " + w)
+//    }
+//
+//    val trainTestSimple: SplitTestAndTrain = d.splitTestAndTrain(splitTrainNum, new Random(seed))
+//
+//
+//    log.info("Evaluate model....")
+//    val evalSimple = new Evaluation(outputNum)
+//    val outputSimple: INDArray = modelSimple.output(trainTestSimple.getTest().getFeatureMatrix())
+//    evalSimple.eval(trainTestSimple.getTest().getLabels(), outputSimple)
+//    log.info(evalSimple.stats())
+//
+//    log.info("****************Example finished********************")
+
+
     log.info("Load data....")
-    val irisIter = new IrisDataSetIterator(150, 150)
-    val iris = irisIter.next()
+    val irisIter: IrisDataSetIterator = new IrisDataSetIterator(150, 150)
+    val iris: DataSet = irisIter.next()
     iris.normalizeZeroMeanZeroUnitVariance()
     Nd4j.shuffle(iris.getFeatureMatrix(), new Random(seed), 1)
     Nd4j.shuffle(iris.getLabels(),new Random(seed),1)
